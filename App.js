dd0ea4262c052a97dc56d6d1c53de0a4bcf8e3fc
Anjul <anjulsharma010@gmail.com>
2024-08-26 00:32:17 +0530

    igniting

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return (
+    <>
+      <h1>Anjul Sharma</h1>
+      <h2>My app is ignited</h2>
+    </>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<App />);
